e01384fe8bbeb3eb798d10310974b5d9ed3f7e9d
rolrodriguez <rolandro@hotmail.com>
2020-09-19 20:48:55 -0400

    Added commets for clarity

--- js/main.js
+++ js/main.js
@@ -9,16 +9,21 @@
    */
   function mainContentLinks() {
   
+  // list of objects for the menu
   const links = [
     { "label": "Week 01 notes", "url": "./week01/" }
   ];
 
-  //
+  // Get content list
   var contentList = document.getElementById("contentList");
   for (const i in links) {
-    
+    // Create an li
     var li = document.createElement("li");
+
+    // Add label and link to the item of the list
     li.innerHTML = `<a href="${links[i]["url"]}">${links[i]["label"]}`;
+    
+    // Add the li to the ol
     contentList.appendChild(li);
   }
   }
--- week01/js/WK01.js
+++ week01/js/WK01.js
@@ -1,3 +1,15 @@
+/**
+ *  Functions for W01 localStorage exercise
+ *  @author Rolando Rodriguez
+ */
+
+
+ /**
+  *  Function readStory
+  * 
+  *  Reads the story from localStorage and puts it 
+  *  into the storyEntry textarea using the story title as key
+  */
 function readStory() {
   const title = document.getElementById("title");
   const storyEntry = document.getElementById("storyEntry");
@@ -6,12 +18,25 @@ function readStory() {
   
 }
 
+/**
+  *  Function saveStory
+  * 
+  *  Save the story from the storyEntry textarea
+  *  into localStorage using the story title as 
+  *  the key
+  */
 function saveStory() {
   const title = document.getElementById("title");
   const storyEntry = document.getElementById("storyEntry");
   localStorage.setItem(title.value, storyEntry.value);
 }
 
+/**
+  *  Function displayStory
+  * 
+  *  Puts the text from the storyEntry textarea into the
+  *  entries section of the website
+  */
 function displayStory() {
   const storyEntry = document.getElementById("storyEntry");
   document.getElementById("entries").innerHTML = storyEntry.value;
